b6d94a48709a74c7ed0a97bc166f1f4e51f21bbf
lixiuye <hai30@163.com>
2019-06-20 17:41:56 +0800

    update

--- resources/views/research/team.blade.php
+++ resources/views/research/team.blade.php
@@ -113,7 +113,6 @@
                         <img data-original="img/ct.png" />
                     </div>
                     <p><br/></p>
-                    <p><br/></p>
                     <p class="title">
                         <strong>陈炯	 1973.12 副教授</strong>
                     </p>
@@ -129,21 +128,22 @@
                 <div class="avatar">
                     <img src="img/lbd.png"/>
                 </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>李兵弟 教授级高级城市规划师</strong></p>
                     <p class="title"><strong>中国城镇化促进会副主席</strong></p>
-                    <p>教授级高级城市规划师，曾连续多年参加中共中央1号文件起草工作。现任中国城镇化促进会副主席，兼城市与乡村统筹发展专业委员会主任、特色小镇委员会常务副会长，曾任住房和城乡建设部村镇建设司司长，建设部村镇建设办公室主任、城乡规划司副司长、巡视员，中国城市规划设计研究院副院长。享受国务院特殊津贴专家待遇。</p><p><br/></p><p><br/></p>
+                    <p>教授级高级城市规划师，曾连续多年参加中共中央1号文件起草工作。现任中国城镇化促进会副主席，兼城市与乡村统筹发展专业委员会主任、特色小镇委员会常务副会长，曾任住房和城乡建设部村镇建设司司长，建设部村镇建设办公室主任、城乡规划司副司长、巡视员，中国城市规划设计研究院副院长。享受国务院特殊津贴专家待遇。</p>
+                    <p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/zfy.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>张夫也 清华大学美术学院教授 博士生导师</strong></p>
                     <p class="title"><strong>清华大学美术学院世界艺术史研究所所长</strong></p>
                     <p>清华大学美术学院教授、博士生导师，清华大学美术学院世界艺术史研究所所长。兼任中国艺术研究院特聘教授、博士生导师，中国工艺美术研究院专家指导委员会委员，北京工艺美术协会副会长,西南交通大学特聘教授、博士生导师。曾任日本东京艺术大学客座研究员，中国艺术类核心期刊《装饰》杂志主编，清华大学美术学院艺术史论学部主任。</p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/zyj.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>张耀军 1968.05</strong></p>
                     <p class="title"><strong>教授中国人民大学社会与人口学院</strong></p>
                     <p class="title"><strong>社会人口</strong></p>
@@ -151,14 +151,14 @@
                     <div class="avatar">
                     <img src="img/sj.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>孙君 北京绿十字总顾问</strong></p>
                     <p class="title"><strong>中国人民大学艺术学院特聘教授</strong></p>
                     <p>北京绿十字总顾问、中国人民大学艺术学院特聘教授，其建设的美丽乡村有：信阳市郝堂村、襄阳市堰河村、广水市桃源村、十堰市樱桃沟村、雅安市雪山村与戴维村、怀化市高椅古村、汝城县金山古村、荆门市太子山小镇、安徽三瓜公社等，目前已成为中国乡村建设的明星村。2010年获得“2009绿色中国年度人物”称号。</p><p><br/></p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/zg.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>郑国 1977.04</strong></p>
                     <p class="title"><strong>副教授中国人民大学公共管理学院</strong></p>
                     <p class="title"><strong>公共管理</strong></p>
@@ -166,7 +166,7 @@
                     <div class="avatar">
                     <img src="img/hb.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>黄波 1981.03 副教授</strong></p>
                     <p class="title"><strong>中国人民大学农业与农村发展学院</strong></p>
                     <p class="title"><strong>农旅与农经</strong></p>
@@ -174,21 +174,21 @@
                     <div class="avatar">
                     <img src="img/zzl.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>郑志良 1969.01 副教授</strong></p>
                     <p class="title"><strong>中国人民大学文学院	 古代文学</strong></p>
                     <p>中国人民大学文学院副教授，南京大学文学院博士，北京大学中文系博士后，古代文学教研室主任。中国俗文学学会常务理事、中国儒林外史学会副秘书长、汤显祖国际研究中心客座研究员。获“中国人民大学杰出人文青年学者”称号，科研成果获北京市第十四届哲学社会科学优秀成果二等奖。</p><p><br/></p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/sxy.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>孙晓阳 北京绿十字主任</strong></p>
                     <p class="title"><strong>湖南农道公益基金会理事长</strong></p>
                     <p>北京绿十字主任，湖南农道公益基金会理事长，农道联众（北京）城乡规划设计研究院有限公司执行院长，清华大学美丽乡村公益基金清农学堂教育长，台湾辅仁大学智慧旅游学院特聘高级讲师，中国民俗学会中国乡愁文化发展研究中心专家委员会副主任。《北京绿十字通讯》主编，《北京青年报》与英国大使馆“环保明星”评委，半汤乡学院特聘研究员。</p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/wwh.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>王伟华</strong></p>
                     <p class="title"><strong>湖北省村镇建设协会理事长</strong></p>
                     <p class="title"><strong>湖北省文化产业学会副会长</strong></p>
@@ -196,42 +196,42 @@
                     <div class="avatar">
                     <img src="img/wjh.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>王京红 中央美术学院博士</strong></p>
                     <p class="title"><strong>留法色彩学者 国家一级注册建筑师</strong></p>
                     <p>中央美术学院博士，留法色彩学者，国家一级注册建筑师。全国颜色标准化技术委员会（SAC/TC120）委员。中国社会科学院城市营销与品牌化智库专家，年度《中国城市营销发展报告》撰稿人之一。国家人保部“色彩搭配设计师”的职业标准制定和评审专家。中国流行色协会理事、建筑与环境色彩研究专业委员会委员、色彩教育专业委员会委员。中国国际色彩联盟专家委员。  </p><p><br/></p><p><br></p>
                     <div class="avatar">
                     <img src="img/lxj.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>鲁晓静 中央美术学院建筑系毕业 LUCIELALUNE </strong></p>
                     <p class="title"><strong>路西的月亮生活美学社-传统手工时尚设计品牌创始人</strong></p>
                     <p>中央美术学院建筑系毕业，LUCIELALUNE 路西的月亮生活美学社-传统手工时尚设计品牌创始人，北京MHOUSE毎屋, 低碳模块移动装配集成建筑设计，合伙创始⼈、设计总监。曾任土人生态城市设计中心建筑总工，北京中国科学建筑设计有限公司首席建筑师。</p><p><br/></p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/hw.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>何崴</strong></p>
                     <p class="title"><strong>中央美术学院建筑学院副教授 实验中心主任 </strong></p>
                     <p>中央美术学院建筑学院副教授，实验中心主任、中国建筑学会建筑师分会乡村建筑专委会副主任委员、中国建筑学会建筑师分会地区建筑专委会委员、中国民族建筑研究会美丽乡村协同建设专委会委员、《小城镇建设》杂志编委。</p><p><br/></p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/zhx.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>赵海翔 1973.05 副教授</strong></p>
                     <p class="title"><strong>中央民族大学美术学院 建筑设计</strong></p>
                     <p>中央民族大学副教授、硕导，文化部恭王府中华传统技艺研究与保护中心研究员。近年完成《少数民族建筑中的民族性探析》、《现代欧洲城市建筑对历史文化场所的重塑》、《纪念性空间研究》、《云南古建筑地图》等课题和专著；2014年起任住建部传统民居保护专委会工作组成员，参加了《中国传统民居类型全集》和《中国传统建筑解析与传承》等课题工作</p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/zcy.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>周存玉 国家一级美术师</strong></p>
                     <p class="title"><strong>研究员级高级工艺美术师 中国工艺美术协会常务理事</strong></p>
                     <p>国家一级美术师，研究员级高级工艺美术师，中国工艺美术协会常务理事，江苏省非遗锻铜技艺代表性传承人，江苏省工艺美术大师，江苏省工艺美术学会副会长，江苏省省工艺美术行业协会副会长，中国非遗传承人产业基地艺术总监；世界技能大赛建筑石雕项目中国赛区裁判。其绘画作品获2015米兰世博会艺术金奖；其锻铜作品于2013年至2015年连续三届获中国工艺美术大师精品博览会金奖。主要从事书画、雕塑、陶艺的创作，是一位跨界艺术家。</p><p><br/></p><p><br/></p>
                     <div class="avatar">
                     <img src="img/txg.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>唐晓刚 中国人民大学艺术学院</strong><br/></p>
                     <p>中国人民大学艺术学院，2003年毕业于东京学艺大学，获得硕士学位。研究方向主要是设计概论,VI设计，广告策划设计。</p><p><br/></p><p><br/></p><p><br/></p><p><br/></p>
                     <div class="avatar">
@@ -245,14 +245,13 @@
                     <div class="avatar">
                     <img src="img/wp.png"/>
                     </div>
-                    <p><br/></p><p><br/></p><p><br/></p>
+                    <p><br/></p>
                     <p class="title"><strong>王鹏 著名文化产业策划专家</strong></p>
                     <p class="title"><strong>北大青鸟文化艺术研究院院长</strong></p>
                     <p>著名文化产业策划专家，北大青鸟文化艺术研究院院长、中国人民大学文化艺术策划研究所时任副所长，兼任民委中国民族品牌文化研究院院长、文化部特聘职业教育导师、中国投资协会特色小镇评审专家、2019世界北京园博会大型活动特聘策划专家、多届东盟博览会开幕大会担任策划专家。多次策划国家级大型文化活动。近年来在全国近20多个文旅景区和特色小镇、文旅产业等进行战略、定位、内容、运营、品牌营销策划和前期投资对接服务。2015年荣获"品牌中国功勋人物"奖。</p>
                 <p><br/></p>
                 <p><br/></p>
                 <p><br></p>
-
                 </span>
             </div>
         </div>
